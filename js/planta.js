3a94b341c32cd2d1c8736814178ae82851fa53f6
Jas <jas.on32823@gmail.com>
2026-07-01 01:26:34 -0600

    Versión 2.0
    Se remplazaron los place holder, se corrigieron algunas plantas duplicadas y nombres mal escritos, se agregó un alt que muestra el nombre de la planta en caso de no haber imagen o no cargar,  se estilizó la pagina

--- js/planta.js
+++ js/planta.js
@@ -5,27 +5,12 @@ const id = Number(parametros.get("id"));
 const planta = plantas.find((p) => p.id === id);
 
 if (planta) {
-  document.getElementById("nombre").textContent = planta.comun;
-
-  document.getElementById("cientifico").textContent = planta.cientifico;
-
-  document.getElementById("origen").textContent = planta.origen;
-
-  document.getElementById("ubicacion").textContent = planta.ubicacion;
-
-  document.getElementById("descripcion").textContent = planta.descripcion;
-
   document.getElementById("imagen").src = planta.imagen;
+  const imagen = document.getElementById("imagen");
 
+  imagen.src = planta.imagen;
+  imagen.alt = planta.cientifico;
   const lista = document.getElementById("usos");
-
-  planta.usos.forEach((uso) => {
-    const li = document.createElement("li");
-
-    li.textContent = uso;
-
-    lista.appendChild(li);
-  });
 } else {
   document.body.innerHTML = "<h1>Planta no encontrada</h1>";
 }
